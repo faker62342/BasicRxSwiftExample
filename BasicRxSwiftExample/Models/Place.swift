f570d42215c1504260e01d6d032fe31c89f9b76a
Mohamed Ali BELHADJ <medalibelhadj2@gmail.com>
2019-05-10 10:05:33 +0100

    create place model && bind tableView to array of data using rx && creating event on each tableView element

---
+++ BasicRxSwiftExample/Models/Place.swift
@@ -0,0 +1,15 @@
+//
+//  Place.swift
+//  BasicRxSwiftExample
+//
+//  Created by Mohamed Ali BELHADJ on 10/05/2019.
+//  Copyright © 2019 Hassine Faker. All rights reserved.
+//
+
+import Foundation
+
+struct Place: Decodable {
+    let name: String
+    let desc: String
+    let url: String
+}
